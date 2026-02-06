58207a425de402deb576b76c5d6e54ae0a8436b4
supunyasantha <supunyasantha.official@gmail.com>
2026-02-06 16:20:06 +0530

    ecoTips for home screen

---
+++ src/constants/ecoTips.tsx
@@ -0,0 +1,34 @@
+export const ECO_TIPS = [
+  {
+    id: "1",
+    title: "Save Energy",
+    description:
+      "Switch off lights when leaving a room to reduce carbon emissions.",
+    icon: "zap",
+    color: "#EAB308",
+  },
+  {
+    id: "2",
+    title: "Reduce Plastic",
+    description:
+      "Use a reusable cloth bag instead of plastic bags when shopping.",
+    icon: "shopping-bag",
+    color: "#3B82F6",
+  },
+  {
+    id: "3",
+    title: "Conserve Water",
+    description:
+      "Turn off the tap while brushing your teeth to save up to 6 liters per minute.",
+    icon: "droplet",
+    color: "#06B6D4",
+  },
+  {
+    id: "4",
+    title: "Plant a Tree",
+    description:
+      "Trees absorb CO2 and release oxygen. Plant one in your backyard today!",
+    icon: "tree-pine",
+    color: "#22C55E",
+  },
+];
