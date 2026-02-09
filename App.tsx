63404ae3a042b593fac125ab0563b00a3f1e9cfd
supunyasantha <supunyasantha.official@gmail.com>
2026-02-09 13:41:29 +0530

    import Privacy and Helpcenter screens.

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@ import { StatusBar } from "expo-status-bar";
 
 import LoginScreen from "./src/screens/Auth/LoginScreen";
 import TabNavigator from "./src/navigation/TabNavigator";
+import PrivacyPolicyScreen from "./src/screens/Settings/PrivacyPolicyScreen";
+import HelpCenterScreen from "./src/screens/Settings/HelpCenterScreen";
+
 import { UserProvider } from "./src/context/UserContext";
 
 const Stack = createNativeStackNavigator();
@@ -25,6 +28,16 @@ export default function App() {
           >
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Home" component={TabNavigator} />
+            <Stack.Screen
+              name="PrivacyPolicy"
+              component={PrivacyPolicyScreen}
+              options={{ headerShown: true, title: "Privacy Policy" }}
+            />
+            <Stack.Screen
+              name="HelpCenter"
+              component={HelpCenterScreen}
+              options={{ headerShown: true, title: "Help Center" }}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
