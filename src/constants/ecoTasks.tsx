37ce43c8db50fe1a4fd4d5eab1f77780db421cc8
supunyasantha <supunyasantha.official@gmail.com>
2026-02-09 13:42:09 +0530

    Eco_Tasks.

---
+++ src/constants/ecoTasks.tsx
@@ -0,0 +1,8 @@
+export const ECO_TASKS = [
+  { id: "1", task:"Used a reusable water bottle", points:10 },
+  { id: "2" , task:"Turned off lights when leaving room" ,points: 5},
+  { id: "3" , task:"Avoided single-use plastics", points:15},
+  { id: "4" ,task:"Walked or cycled for short distance", points:20},
+  { id: "5" , task:"Used a reusable bag for shopping", points:10},
+
+];
